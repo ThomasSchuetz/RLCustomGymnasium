cc7e60f9e01dda6de70e0120a8a61d03c9d05192
ThomasSchuetz <t.schuetz.1990@gmail.com>
2023-04-23 13:26:42 +0200

    enha: remove hard-coded, custom directory for stored training checkpoints

--- apply_policy.py
+++ apply_policy.py
@@ -5,7 +5,7 @@ from ray.tune.registry import register_env
 register_env(env_id, lambda _: CartpoleGym())
 
 from ray.rllib.algorithms.algorithm import Algorithm
-latest_checkpoint_dir = "C:\\Users\\tschu\\ray_results\\PPO_cartpole_2023-04-19_20-44-10hz6alfwf\\checkpoint_000005"
+latest_checkpoint_dir = "" # something like "$HOME/ray_results/PPO_cartpole_2023-04-19_20-44-10hz6alfwf/checkpoint_000005"
 agent = Algorithm.from_checkpoint(latest_checkpoint_dir)
 env = gym.make(env_id)
 state, info = env.reset()
